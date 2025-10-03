d890e6b8267fd8a705137a08cfee5ad3bd9bce2e
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-03 22:13:13 +0300

    reassinging and incrementing variables and values

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-// //document.getElementById("count-el").innerText = 5;
-// // let firstBatch = 5;
-// // let secondBatch = 7;
+// let count = 5;
+// count = count + 1
+// console.log(count);
 
-// // let count = firstBatch + secondBatch;
-// console.log(count)
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
 
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
 
-let myAge = 32;
-let humanDogRatio = 7;
 
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge)
