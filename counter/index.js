fd6f1ccdb420ecd37ebc6d9a69e790d34f26c6f6
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 17:15:34 +0300

    create save feature

--- counter/index.js
+++ counter/index.js
@@ -1,16 +1,21 @@
+// 1. Grab the save-el paragraph and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el");
 let count = 0;
 
+console.log(saveEl)
+
 function increment() {
-  // change this to plus equal technique
-  // count = count + 1;
   count += 1;
   countEl.innerText = count;
 }
 
 function save() {
-  console.log(count);
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let countStr = count + " - ";
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.innerText += countStr;
+  // NB: Make sure to not delete the existing content of the paragraph
+
+  // console.log(count);
 }
-
-
-
