1467569edb75a28ba3c8439d7e3b969e61d79286
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 07:18:19 +0300

    function that logs the sums

--- 01. function3/index.js
+++ 01. function3/index.js
@@ -1,6 +1,13 @@
-// create a function that logs out the number 42 to console
-function myLogger() {
-  console.log(42);
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+
+// create a function that logs out sum of all the lap times
+function logLapTime() {
+  let totalTime = lap1 + lap2 + lap3;
+  console.log(totalTime);
 }
-// call the function
-myLogger(42);
+
+logLapTime();
+console.log(totalTime)
