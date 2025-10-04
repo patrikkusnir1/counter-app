81a64ef442417a8436c7a6d793bd2cc8c2ea6841
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 07:43:33 +0300

    display the count

--- counter/index.js
+++ counter/index.js
@@ -1,17 +1,17 @@
 // document.getElementById("count").innerText = 5;
 
-// initialize the count as 0
-
-// listen for clicks on the increment button
-
-// increment the count variable when the button is clicked
-
 // change the count-el in the HTML to reflect to new count
+
+//camelCase
+let countEl = document.getElementById("count-el"); // pass-in arguments
+
+console.log(countEl);
+
 let count = 0;
 
 function increment() {
   count = count + 1;
-  console.log(count);
+  countEl.innerText = count;
 }
 
 
