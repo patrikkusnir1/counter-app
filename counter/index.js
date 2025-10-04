3b867fcc83fbb5e3ce1c3385b41ab330c56c1838
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 17:23:06 +0300

    fix problem with innerHTMl - use textContent

--- counter/index.js
+++ counter/index.js
@@ -7,15 +7,11 @@ console.log(saveEl)
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
   let countStr = count + " - ";
-  // 3. Render the variable in the saveEl using innerText
-  saveEl.innerText += countStr;
-  // NB: Make sure to not delete the existing content of the paragraph
+  saveEl.textContent += countStr;
 
-  // console.log(count);
 }
