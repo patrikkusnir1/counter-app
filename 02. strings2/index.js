2bd1e149feb885adeabce221d9d0e26cabae49cd
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 16:20:27 +0300

    create greeting for yourself

---
+++ 02. strings2/index.js
@@ -0,0 +1,8 @@
+// create two variables, name and greeting. The name variable should store your name, and the greeting 
+// should store "Hi, my name is ..."
+// create a third variable, myGreeting that contatenates that 2 strings 
+// log myGreeting to console
+let name = "Patrik Kusnir";
+let greeting = "Hi, my name is "; // also without space but add into myGreeting space " "
+let myGreeting = greeting + name;
+console.log(myGreeting)
