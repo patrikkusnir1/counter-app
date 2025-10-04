11d8c0380fbd9684a18d7134e78acf482d5fa0d9
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 07:35:01 +0300

    count onclick function

--- counter/index.js
+++ counter/index.js
@@ -1,18 +1,18 @@
-// let count = 5;
-// count = count + 1
-// console.log(count);
-
-// Pseudocode
+// document.getElementById("count").innerText = 5;
 
 // initialize the count as 0
+
 // listen for clicks on the increment button
+
 // increment the count variable when the button is clicked
+
 // change the count-el in the HTML to reflect to new count
+let count = 0;
 
 function increment() {
-  console.log("Button was clicked")
+  count = count + 1;
+  console.log(count);
 }
-
 
 
 
