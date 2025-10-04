b4827b6bf01638527478f75442a2ead2bd8ce573
Patrik Kusnir <patrikkusnir1@gmail.com>
2025-10-04 16:00:50 +0300

    learn strings, how to create and concatenate

---
+++ 02. strings/index.js
@@ -0,0 +1,7 @@
+let username = "per"; // string
+// create variable, message that stores the string: "You have three new notifications"
+let message = "You have three notifications";
+
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ", " + username + "!";
+console.log(messageToUser);
