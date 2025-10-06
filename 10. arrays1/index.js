9c9736dcb2add81b80bfce6967b4c40164153554
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 17:38:50 +0300

    learn about .length method

--- 10. arrays1/index.js
+++ 10. arrays1/index.js
@@ -3,20 +3,9 @@
 // 0 indexed
 
 let featuredPosts = [
-  "Check out my Netflix clone", // 0 
-  "Here's the code for my project",// 1
-  "I've just relaunched my portfolio"// 2
+  "Check out my Netflix clone",       // 0 
+  "Here's the code for my project",   // 1
+  "I've just relaunched my portfolio" // 2
 ];
 
-// console.log(featuredPosts[2])
-
-let experience = ["CEO at Scrimba","Frontend developer at Xeneta", "People counter at Norstat", "Javascript programming"];
-
-// Make the following appear in the console:
-// Frontend developer at Xeneta
-// People counter for Norstat
-// CEO at Scrimba
-
-console.log(experience[1])
-console.log(experience[2])
-console.log(experience[0])
+console.log( featuredPosts.length );
