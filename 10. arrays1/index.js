80ec761bbb83d39618f3577ecf01f6fe2db88d10
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 17:36:32 +0300

    console.log experience array based on index

--- 10. arrays1/index.js
+++ 10. arrays1/index.js
@@ -1,9 +1,22 @@
 // arrays - ordered lists of items
-let featuredPosts = ["Check out my Netflix clone",
-                      "Here's the code for my project",
-                      "I've just relaunched my portfolio"
+
+// 0 indexed
+
+let featuredPosts = [
+  "Check out my Netflix clone", // 0 
+  "Here's the code for my project",// 1
+  "I've just relaunched my portfolio"// 2
 ];
 
-// Create an array that lists your i.e. experience, education, licenses, skills or similar
-// The items of the array should be strings
-let myExperience = ["pharmacology", "Quality assessment", "Toxicology", "Javascript programming"]
+// console.log(featuredPosts[2])
+
+let experience = ["CEO at Scrimba","Frontend developer at Xeneta", "People counter at Norstat", "Javascript programming"];
+
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
