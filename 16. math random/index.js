f3e93e7d238fb65c1d57526df68563e268d37e02
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-07 11:54:45 +0300

    learn about math.random

---
+++ 16. math random/index.js
@@ -0,0 +1,9 @@
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+/* 
+
+What does Math.random() do?
+Your answer: generates random  number between including 0 and 1, which is not included. 
+
+*/
