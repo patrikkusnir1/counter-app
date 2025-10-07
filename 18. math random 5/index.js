cb2407c0ec5ea0548c96e6337d094d096d07b32e
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-07 17:34:36 +0300

    create a person object and function that calls it

--- 18. math random 5/index.js
+++ 18. math random 5/index.js
@@ -1,8 +1,8 @@
 // Try to modify expression so that we get range from 1 to 6
-// let randomNumber = Math.floor( Math.random() * 6) + 1;
-
+ let randomNumber = Math.floor( Math.random() * 6) + 1;
+console.log(randomNumber);
 // Create a function, rollDice(), that returns a random number between 1 and 6
-// console.log(randomNumber)
+
 
 function rollDice(){
   let randomNumber = Math.floor(Math.random() * 6) + 1;
