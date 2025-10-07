c4708dccb4f41b42cf47aa2106eb7f4654c5f8b0
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-07 16:49:32 +0300

    create object about renting castle

---
+++ 20. objects2/index.js
@@ -0,0 +1,22 @@
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let castleRent = {
+  name: "Abercrave",
+  price: 73,
+  dates: ["7.10.2025", "8.10.2025","9.10.2025"],
+  free: true
+};
+
+console.log(castleRent.price)
+console.log(castleRent.free)
+
+let castle = {
+  title: "Live like king in my castle",
+  price: 190,
+  isSuperhost: true,
+  images: ["img/castle1.png", "img/castle2.png"],
+};
+console.log(castle.price)
+console.log(castle.isSuperhost)
