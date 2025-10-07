16f06d1a282a0b7bd3d6c4dc5a8fa18d5aa501c0
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-07 17:52:18 +0300

    for loop for log out countries with dash

---
+++ 10. loop and arrays practice/index.js
@@ -0,0 +1,16 @@
+let largeCountries = ["China", "India", "United States", "Indonesia", "Pakistan"]
+
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indonesia
+- Pakistan
+*/
+console.log("The 5 largest countries in the world:");
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log("- " + largeCountries[i])
+}
