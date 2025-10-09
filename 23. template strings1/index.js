6aed2738fe087139dbc25967f44a7e7ed33ebc5a
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 07:58:13 +0300

    add sender variable and log it in string

--- 23. template strings1/index.js
+++ 23. template strings1/index.js
@@ -1,8 +1,8 @@
 // template strings/literals
 const recipient = "James";
+const sender = "Patrik Kusnir";
+// Create a new variable, sender, and set its value to your name
+// Use your sender variable instead of "Per"
 
-
-// Refactor the email string to use template strings
-// const email = "Hey " + recipient + "! How is it going? Cheers Per";
-const email = `Hey ${recipient}! How is it going? Cheers Per`;
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
 console.log(email)
