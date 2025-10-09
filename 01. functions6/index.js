43ac362e5a20276d84f793adbe187361bdf78b9c
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 18:10:14 +0300

    practice functions with parameters

---
+++ 01. functions6/index.js
@@ -0,0 +1,7 @@
+const welcomeEl = document.getElementById("welcome-el");
+// Give the function a parameter, greeting, that replaces "Welcome back"
+function greetUser(greeting) {
+  welcomeEl.textContent = greeting + ", Patrik 👋";
+}
+
+greetUser("Hello")
