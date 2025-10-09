080f720317172eaddfefc8ef14cfcc18188689c4
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 17:11:43 +0300

    test truthy or falsy values

--- 25. truthy and falsy values2/index.js
+++ 25. truthy and falsy values2/index.js
@@ -1,47 +1,7 @@
-// const credits = 0; // 0 is falsy value = translate to false
-
-// if (credits) { // JS converts it as true
-//   console.log("let's play")
-// } else {
-//   console.log("Sorry, you have no credits.")
-// }
-
-// truthy
-// falsy
-
-// falsy values: 
-/* 
-  - false
-  - 0
-  - ""
-  - null => how me as developer signilize emptyness
-  - undefined => JS tells me that it is undefined
-  - NaN
-*/
-
-// let currentViewers = null;
-
-// currentViewers = ["Jane", "Nick"];
-
-// currentViewers = null; // signilize emptyness from my side
-
-// if (currentViewers) {
-//   // .... notify the streamers
-//   console.log("We have viewers, log in!")
-// }
-
-// let currentViewers = ["jane"];
-// console.log(currentViewers[5]) - returns undefined
-
-
-// if (["dsdsdsd","dsdsddsds", "dsdsdsds"]) { // JS converts arrays as true - truthy value
-//   console.log("let's play")
-// } else {
-//   console.log("Sorry, you have no credits.")
-// }
-
-// if ("") { // JS converts arrays as false - falsy value
-//   console.log("let's play")
-// } else {
-//   console.log("Sorry, you have no credits.")
-// }
+// check if value is truthy or falsy
+console.log( Boolean("")  ); // false
+console.log( Boolean("0") ); // true
+console.log( Boolean(100) ); // true
+console.log( Boolean(null)); // false
+console.log( Boolean([0]) ); // true
+console.log( Boolean(-0)  ); // false
