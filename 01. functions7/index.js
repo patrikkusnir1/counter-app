df1d9516d726ade37f0d1552ef610353a992384c
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 18:48:24 +0300

    learn difference between parameters and arguments

--- 01. functions7/index.js
+++ 01. functions7/index.js
@@ -1,8 +1,19 @@
-// Create a function, add(), that adds two numbers together and returns the sum
+// What are greeting and name? // parameters
+// What are "Howdy" and "James"? // arguments
+// What are num1 and num2? // parameters
+// What are 3 and 4? // arguments
+
+const welcomeEl = document.getElementById("welcome-el");
+//                 parameters - inside the function
+function greetUser(greeting, name) {
+  welcomeEl.textContent = `${greeting}, ${name} 🔥`;
+}
+//        arguments - outside the function
+let hi = "Howdy";
+greetUser(hi, "James");
 
 function add(num1, num2) {
   return num1 + num2;
 }
 
-console.log(add(3, 4)) // should log 7
-console.log(add(9, 102)) // should log 111
+add(3, 4);
