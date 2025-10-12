e96fd95f6e77ccaaddf5753b1a67a7f2e38517af
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-12 17:08:16 +0300

    practice difference between let and const, template strings

---
+++ 10. arrays as parameters/index.js
@@ -0,0 +1,8 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+  return arr[0];
+}
+let firstCard = getFirst([10, 2, 5]); // blackjack game cards
+
+console.log(firstCard)
+// Call it with an array as an argument to verify that it works
